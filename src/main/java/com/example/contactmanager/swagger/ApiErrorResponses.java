4cd6589d795930d270426249add3143dfab8f90e
gui <gui151208@gmail.com>
2026-03-15 18:34:09 -0300

    .

---
+++ src/main/java/com/example/contactmanager/swagger/ApiErrorResponses.java
@@ -0,0 +1,30 @@
+package com.example.contactmanager.swagger;
+
+import com.example.contactmanager.exception.ApiError;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponses({
+        @ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized",
+                content = @Content(schema = @Schema(implementation = ApiError.class))
+        ),
+        @ApiResponse(
+                responseCode = "409",
+                description = "Conflict",
+                content = @Content(schema = @Schema(implementation = ApiError.class))
+        ),
+        @ApiResponse(
+                responseCode = "500",
+                description = "Internal server error",
+                content = @Content(schema = @Schema(implementation = ApiError.class))
+        )
+})
+public @interface ApiErrorResponses {
+}
