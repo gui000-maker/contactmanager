6ec5c47f1b0a67a5c250c8840806785b114f1eb2
gui <gui151208@gmail.com>
2026-03-27 16:33:28 -0300

    add transactional and modifying in RefreshTokenRepository method

--- src/main/java/com/example/contactmanager/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/contactmanager/repository/RefreshTokenRepository.java
@@ -3,6 +3,8 @@ package com.example.contactmanager.repository;
 import com.example.contactmanager.entity.RefreshToken;
 import com.example.contactmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,5 +12,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByToken(String token);
 
+    @Modifying
+    @Transactional
     void deleteByUser(User user);
 }
